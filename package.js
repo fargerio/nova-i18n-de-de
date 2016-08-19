2fe6787a9234f167d1c4be71e5c0b5aefadb4c3b
Florian Zeidler <zeidler@klimakoenner.de>
2016-08-19 11:36:43 +0200

    changed package name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "fargerio:nova-i18n-de-de",
+  name: "fzeidler:nova-i18n-de-de",
   summary: "Telescope i18n package (de_DE)",
   version: "0.26.5-nova",
   git: "https://github.com/fargerio/nova-i18n-de-de"
